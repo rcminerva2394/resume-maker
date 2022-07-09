4256f730ea3178b42245a17c91f3e853359636bc
jeffrey125 <jeffreyasilo8@gmail.com>
2022-07-09 17:37:15 +0800

    refactor: Made a new Input component

--- src/components/EditForm/Experience/ExperienceItem.js
+++ src/components/EditForm/Experience/ExperienceItem.js
@@ -2,38 +2,46 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Button from '../../Utils/Button';
+import Input from '../../../UI/Input';
 
 const ExperienceItem = ({ id, experienceItem, onChange, onDelete }) => {
+  const INPUT_DATA = [
+    {
+      type: 'text',
+      name: 'position',
+      placeholder: 'Job Role',
+      onChange: (e) => onChange(e, id),
+      value: experienceItem.position,
+    },
+    {
+      type: 'text',
+      name: 'company',
+      placeholder: 'Company',
+      onChange: (e) => onChange(e, id),
+      value: experienceItem.company,
+    },
+    {
+      type: 'text',
+      name: 'from',
+      placeholder: 'From',
+      onChange: (e) => onChange(e, id),
+      value: experienceItem.from,
+    },
+    {
+      type: 'text',
+      name: 'to',
+      placeholder: 'To',
+      onChange: (e) => onChange(e, id),
+      value: experienceItem.to,
+    },
+  ];
+
   return (
     <ExperienceItemWrapper>
-      <input
-        type="text"
-        name="position"
-        placeholder="Job Role"
-        onChange={(e) => onChange(e, id)}
-        value={experienceItem.position}
-      ></input>
-      <input
-        type="text"
-        name="company"
-        placeholder="Company"
-        onChange={(e) => onChange(e, id)}
-        value={experienceItem.company}
-      ></input>
-      <input
-        type="text"
-        name="from"
-        placeholder="From"
-        onChange={(e) => onChange(e, id)}
-        value={experienceItem.from}
-      ></input>
-      <input
-        type="text"
-        name="to"
-        placeholder="To"
-        onChange={(e) => onChange(e, id)}
-        value={experienceItem.to}
-      ></input>
+      {/* You can use this para DRY code mo */}
+      {INPUT_DATA.map((inData) => (
+        <Input key={inData.name} inData={inData} />
+      ))}
       <textarea
         name="description"
         placeholder="Description in 200 characters"
