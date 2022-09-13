8f0825f4dcbfa5459f9bca1d69cb695f92e05050
rcminerva2394 <rosebahan@gmail.com>
2022-09-13 17:06:42 +0800

    Edited tools to Technical Skills

--- src/components/EditForm/Skills/Skills.js
+++ src/components/EditForm/Skills/Skills.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React from 'react';
 
-import SectionWrapper from "../../Utils/SectionWrapper";
-import AddSkill from "./AddSkill";
-import SkillsList from "./SkillsList";
+import SectionWrapper from '../../Utils/SectionWrapper';
+import AddSkill from './AddSkill';
+import SkillsList from './SkillsList';
 
 const Skills = ({ skills }) => {
-  return (
-    <SectionWrapper title={"Skills"} isLast={true}>
-      <AddSkill title={"Tools"} placeholder={"Add a new tool"} type={"tools"} />
-      <SkillsList type={"tools"} skills={skills.tools} />
-      <AddSkill
-        title={"Soft Skills"}
-        placeholder={"Add a new skill"}
-        type={"soft"}
-      />
-      <SkillsList type={"soft"} skills={skills.soft} />
-    </SectionWrapper>
-  );
+    return (
+        <SectionWrapper title={'Skills'} isLast={true}>
+            <AddSkill
+                title={'Technical Skills'}
+                placeholder={'Add a new tool'}
+                type={'tools'}
+            />
+            <SkillsList type={'tools'} skills={skills.tools} />
+            <AddSkill
+                title={'Soft Skills'}
+                placeholder={'Add a new skill'}
+                type={'soft'}
+            />
+            <SkillsList type={'soft'} skills={skills.soft} />
+        </SectionWrapper>
+    );
 };
 
 export default Skills;
--- src/components/PrevForm/PrevForm.js
+++ src/components/PrevForm/PrevForm.js
@@ -1,78 +1,98 @@
-import React from "react";
+import React from 'react';
 
-import SideBar from "../Utils/SideBar";
-import SectionWrapper from "../Utils/SectionWrapper";
-import Skills from "./Skills";
-import WebSocItem from "./WebSocItem";
-import MainContent from "../Utils/MainContent";
-import PersonalInfo from "./PersonalInfo";
-import ExperienceItem from "./ExperienceItem";
-import EducationItem from "./EducationItem";
+import SideBar from '../Utils/SideBar';
+import SectionWrapper from '../Utils/SectionWrapper';
+import Skills from './Skills';
+import WebSocItem from './WebSocItem';
+import MainContent from '../Utils/MainContent';
+import PersonalInfo from './PersonalInfo';
+import ExperienceItem from './ExperienceItem';
+import EducationItem from './EducationItem';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const PrevForm = React.forwardRef(
-  (
-    {
-      photo,
-      aboutMe,
-      socLinks,
-      skills,
-      personalInfo,
-      experience,
-      education,
-    },
-    ref
-  ) => {
-    const experienceItems = experience.map((experienceItem) => (
-      <ExperienceItem key={experienceItem.id} experienceItem={experienceItem} />
-    ));
+    (
+        {
+            photo,
+            aboutMe,
+            socLinks,
+            skills,
+            personalInfo,
+            experience,
+            education,
+        },
+        ref
+    ) => {
+        const experienceItems = experience.map((experienceItem) => (
+            <ExperienceItem
+                key={experienceItem.id}
+                experienceItem={experienceItem}
+            />
+        ));
 
-    const educationItems = education.map((educationItem) => (
-      <EducationItem key={educationItem.id} educationItem={educationItem} />
-    ));
+        const educationItems = education.map((educationItem) => (
+            <EducationItem
+                key={educationItem.id}
+                educationItem={educationItem}
+            />
+        ));
 
-    const socLinkItems = socLinks.map((socLink) => (
-      <WebSocItem link={socLink.link} key={socLink.id} name={socLink.name} />
-    ));
+        const socLinkItems = socLinks.map((socLink) => (
+            <WebSocItem
+                link={socLink.link}
+                key={socLink.id}
+                name={socLink.name}
+            />
+        ));
 
-    return (
-      <BondPaper ref={ref}>
-        <SideBar>
-          <img alt="profile-pic" src={photo ? photo : null}></img>
-          <SectionWrapper title="About Me">
-            <p>{aboutMe}</p>
-          </SectionWrapper>
-          <SectionWrapper title="Website & Social Links">
-            <ul>{socLinkItems}</ul>
-          </SectionWrapper>
-          <SectionWrapper title="Skills" isLast={true}>
-            <Skills skills={skills.tools} title={"Tools"}></Skills>
-            <Skills skills={skills.soft} title={"Soft Skills"}></Skills>
-          </SectionWrapper>
-        </SideBar>
-        <MainContent>
-          <PersonalInfo personalInfo={personalInfo} />
-          <SectionWrapper title="Experience" isMain={true}>
-            {experienceItems}
-          </SectionWrapper>
-          <SectionWrapper title="Education" isMain={true} isLast={true}>
-            {educationItems}
-          </SectionWrapper>
-        </MainContent>
-      </BondPaper>
-    );
-  }
+        return (
+            <BondPaper ref={ref}>
+                <SideBar>
+                    <img alt='profile-pic' src={photo ? photo : null}></img>
+                    <SectionWrapper title='About Me'>
+                        <p>{aboutMe}</p>
+                    </SectionWrapper>
+                    <SectionWrapper title='Website & Social Links'>
+                        <ul>{socLinkItems}</ul>
+                    </SectionWrapper>
+                    <SectionWrapper title='Skills' isLast={true}>
+                        <Skills
+                            skills={skills.tools}
+                            title={'Technical Skills'}
+                        ></Skills>
+                        <Skills
+                            skills={skills.soft}
+                            title={'Soft Skills'}
+                        ></Skills>
+                    </SectionWrapper>
+                </SideBar>
+                <MainContent>
+                    <PersonalInfo personalInfo={personalInfo} />
+                    <SectionWrapper title='Experience' isMain={true}>
+                        {experienceItems}
+                    </SectionWrapper>
+                    <SectionWrapper
+                        title='Education'
+                        isMain={true}
+                        isLast={true}
+                    >
+                        {educationItems}
+                    </SectionWrapper>
+                </MainContent>
+            </BondPaper>
+        );
+    }
 );
 
 const BondPaper = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 210mm;
-  height: ${(props) => (props.isEditForm ? "100%" : "297mm")};
-  background-color: white;
-  border: 1px solid #f5f5f5;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  padding: 20rem 20rem 20rem 20rem;
+    display: flex;
+    flex-direction: row;
+    width: 210mm;
+    height: ${(props) => (props.isEditForm ? '100%' : '297mm')};
+    background-color: white;
+    border: 1px solid #f5f5f5;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    padding: 20rem 20rem 20rem 20rem;
 `;
 export default PrevForm;
